08662ae8f77d17a13849467bd7d850e380e2f46c
Dipesh Kumar <dipshraj@gmail.com>
2022-07-06 11:19:22 +0530

    Handcricket

---
+++ Javscript/javascript.js
@@ -0,0 +1,167 @@
+
+
+let score = 0;
+
+
+
+
+function scoreCalc(userHit) {
+	let x = userHit * 1;
+	let y = computerHit();
+	if (userHit * 1 === y) {
+
+		document.getElementById("outImage").style.visibility = "visible";
+		popupOut();
+	   totalScore.push(score);
+
+		setTimeout(outDelay, 700);
+
+		function outDelay(){
+			// alert("yes");
+			document.getElementById("highScore").innerText=highscore();
+			document.getElementById("highScore1").innerText=highscore();
+
+
+			document.getElementById("score").innerText = score;
+		document.getElementById("finalScore").innerText = score;
+		document.getElementById("resultPage").style.zIndex = "4";
+		document.getElementById("resultPage").style.visibility = "visible";
+	document.getElementById("outImage").style.visibility="hidden";
+
+		}
+	}
+
+	else {
+        
+		if(x===4){
+        	document.getElementById("fourImage").style.visibility="visible";
+			popup4();
+			setTimeout(delay4,1000);
+		}
+
+		if(x===6){
+			document.getElementById("sixImage").style.visibility="visible";
+			popup6();
+			setTimeout(delay6,1000);
+		}
+
+		score = score + x;
+		// score=x;
+		document.getElementById("score").innerText = score;
+		document.getElementById("finalScore").innerText = score;
+
+	}
+
+
+}
+function delay4() {
+	document.getElementById("fourImage").style.visibility="hidden";
+}
+function delay6() {
+	document.getElementById("sixImage").style.visibility="hidden";
+}
+
+
+function computerHit() {
+	const a = Math.floor(Math.random() * 7);
+	if (a == 0 || a == 5) {
+		computerHit();
+	} else {
+		document.getElementById("cInput").innerText = a;
+		return a;
+	}
+}
+// let storeScore[]={};
+
+
+
+function homePage() {
+	// totalScore.push(score);
+	// console.log(highScore);
+	// highscore = score;
+	score = 0;
+	document.getElementById("resultPage").style.zIndex = "1";
+	document.getElementById("resultPage").style.visibility = "hidden";
+	document.getElementById("score").innerText = "Start";
+	document.getElementById("cInput").innerText = "";
+
+
+
+
+
+
+}
+
+// animation
+
+function popupOut() {
+	let id = null;
+	let x = 0;
+	let y=-37;
+	clearInterval(id);
+	id = setInterval(frame, 6);
+	function frame() {
+		const event = document.getElementById("outImage");
+		if (x == 40) {
+			clearInterval(id);
+		} else {
+			x++;
+			y++;
+			event.style.width = x + "vh";
+			event.style.right = y + "vh";
+
+
+		}
+		
+	}
+}
+
+
+
+function popup4(){
+	let id=null;
+	let x=0;
+
+	clearInterval(id);
+	id=setInterval(move4,6);
+	function move4() {
+		const event = document.getElementById("fourImage");
+		if(x == 90){
+			clearInterval(id);
+		}
+		else{
+			x++;
+			event.style.bottom= x + "vh";
+		}
+	}
+}
+
+function popup6(){
+	let id=null;
+	let x=0;
+
+	clearInterval(id);
+	id=setInterval(move6,6);
+	function move6() {
+		const event = document.getElementById("sixImage");
+		if(x == 90){
+			clearInterval(id);
+		}
+		else{
+			x++;
+			event.style.bottom= x + "vh";
+		}
+	}
+}
+
+
+function gameStart(){
+	document.getElementById("imgbox").style.visibility="hidden";
+	document.getElementById("homepage").style.visibility="hidden";
+
+}
+const totalScore=[0];
+function highscore(){
+	return Math.max(...totalScore);
+
+}
